e5e96815fe84b0f9b7a9b9686e33f01a00375e9b
Manasvi Patidar <mpatidar@cs.iitr.ac.in>
2022-02-05 13:12:49 +0530

    init: base repo.

---
+++ test/international_phone_input_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:international_phone_input/international_phone_input.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
